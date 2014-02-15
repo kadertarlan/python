d84c7abb906d64ceabe43d9d739293a044141cd7
kadertarlan <kadertarlan1@gmail.com>
2014-02-15 14:32:04 +0200

    Rename yaş.py to yas.py

---
+++ yas.py
@@ -0,0 +1,9 @@
+yas = int(raw_input("Yas�n�z ka�? "))
+if yas < 0:
+    print "Yok can�m, daha neler!?"
+elif yas <= 30:
+    print "Hmm.. Epey gen�sin."
+elif yas > 30:
+    print "Yaslan�yorsun dostum!"
+elif yas >= 60:
+    print "Yas kemale ermis"
--- yaş.py
+++
@@ -1,9 +0,0 @@
-yas = int(raw_input("Yasınız kaç? "))
-if yas < 0:
-    print "Yok canım, daha neler!?"
-elif yas <= 30:
-    print "Hmm.. Epey gençsin."
-elif yas > 30:
-    print "Yaslanıyorsun dostum!"
-elif yas >= 60:
-    print "Yas kemale ermis"
