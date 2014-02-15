1f387ea3a70db3dfe6e759706ecf35967e268918
kadertarlan <kadertarlan1@gmail.com>
2014-02-15 14:26:24 +0200

    Update and rename dongu.py to bazi_donguler.py

---
+++ bazi_donguler.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+a=0;
+while a<3:
+  a=a+1
+  print a
+print "\n"
+
+
+for i in range(1,10):
+   print i
+
+
+for i in "kelime":
+   print i
+print "\n"
+
+
+for i in range(0,11,2):
+   print i
+print "\n"
+
+
+print range(10,15)
+
+
+print range(0,20,3)
+
+
+
+
